fe2de7465a2621dc1d89ffa3cdc07b2bfd83bd88
gonzo <calleriza.gonzalo@gmail.com>
2023-11-16 18:26:06 +0100

    Processed the conversations, need to clean them

--- advanced_chatbot.py
+++ advanced_chatbot.py
@@ -12,4 +12,28 @@ def read_files():
         line_id_index = 0
         sentence_index = 4
         line_id_to_text[sequences[line_id_index]] = sequences[sentence_index]
+        
+    return line_id_to_text
 
+line_id_to_text = read_files()
+
+def read_conversations():
+    
+    inputs, outputs = [], []
+    
+    with open("movie_conversations.txt", errors="ignore") as conversations:
+        data = conversations.readlines()
+    for line in data:
+        sequences = line.replace("\n", "").split(" +++$+++ ")
+        conversation = [line[1:-1] for line in sequences[3][1:-1].split(", ")]
+        
+        for index in range(len(conversation) - 1):
+            inputs.append(line_id_to_text[conversation[index]])
+            outputs.append(line_id_to_text[conversation[index + 1]])
+            
+            if len(inputs) >= 100000:
+                return inputs, outputs
+            
+    return inputs, outputs
+
+inputs, outputs = read_conversations()
